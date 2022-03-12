cd746530405494adecf02b2de5e31d332a69b773
GunaRakulan-GR <gunarakulan@gmail.com>
2022-03-12 15:42:31 +0530

    Add comments to code

--- face-detection.py
+++ face-detection.py
@@ -1,14 +1,14 @@
 from cvzone.FaceDetectionModule import FaceDetector
 import cv2
 
-cap = cv2.VideoCapture(0)
-detector = FaceDetector()
+cap = cv2.VideoCapture(0)  # Create Video Capture Object
+detector = FaceDetector()  # Create detector instance
 
 while True:
-    success, img = cap.read()
-    img, bboxs = detector.findFaces(img)
+    success, img = cap.read()    # Get Image
+    img, bboxs = detector.findFaces(img)   # Detect Face
 
-    if bboxs:
+    if bboxs:   # If boxes found, 
         # bboxInfo - "id","bbox","score","center"
         center = bboxs[0]["center"]
         cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
@@ -18,4 +18,3 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
-
